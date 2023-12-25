46be10a8bb37eed42259ea9620a1d68922076f37
Moritz Eigenauer <moritz.eigenauer@gmail.com>
2023-12-25 23:40:18 +0100

    Update client_to_exe.bat

--- client_to_exe.bat
+++ client_to_exe.bat
@@ -3,3 +3,10 @@ pyinstaller -F --clean troll1.py -w
 copy dist\troll1.exe troll1.exe
 @RD /S /Q dist
 @RD /S /Q build
+
+pyinstaller -F --clean troll2.py -w
+copy dist\troll2.exe troll2.exe
+@RD /S /Q dist
+@RD /S /Q build
+
+python
